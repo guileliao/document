940900926f0b787d85c686deb5784b851f2621df
guile liao <guileliao@users.noreply.github.com>
2018-01-23 09:43:36 +0800

    Update eg_python_script.py

--- personal/eg_python_script.py
+++ personal/eg_python_script.py
@@ -8,29 +8,29 @@
 ......
 """
 
-__author__ = 'Guile Liao'       #这个变量声明了程序作者
+__author__ = 'Guile Liao'                           #这个变量声明了程序作者
 
-#def funMyFunction():
+#def funMyFunction(): #定义函数
 #       xxx;
 #       xxx;
 #funMyFunction end
 
-import os
-import sys
+import os                                           #导入os模块
+import sys                                          #导入sys模块
 
-strMyVar = "abc"
-numMyNum = 123
-lisMyList = [
+strMyVar = "abc"                                    #定义变量字符串
+numMyNum = 123                                      #定义变量数字
+lisMyList = [                                       #定义列表（数组）
                 'a',
                 2.20,
                 'c'
 ]
-tupMyTuple = (
+tupMyTuple = (                                      #定义元祖（只读列表）
                 't0',
                 't1',
                 't2'
 )
-dicMyDict = {
+dicMyDict = {                                       #定义字典（键值对）
                 't0': 'a',
                 't1': 'b',
                 't2': 3
@@ -41,8 +41,9 @@ def funMyPrint(args):
         return
 #funMyPrint end
 
-for i in range(3):
-        funMyPrint(os.getcwd());        #("/"+raw_input("请输入一个字符串：")));
+for i in range(3):                                   #for循环（有限循环）
+        funMyPrint(raw_input("请输入一个字符串："));
+        funMyPrint(os.getcwd());
         funMyPrint(sys.platform);
         funMyPrint(sys.path);
         funMyPrint(lisMyList[i] * 2);
